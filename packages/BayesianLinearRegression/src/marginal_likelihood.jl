f0d0c54a6b02156e66733fa48bb629700d3f5b3c
Coba <coba@cobac.eu>
2022-05-29 16:38:55 +0200

    cholesky factorization for blm marginal likelihood

--- packages/BayesianLinearRegression/src/marginal_likelihood.jl
+++ packages/BayesianLinearRegression/src/marginal_likelihood.jl
@@ -4,11 +4,11 @@ function marginal_likelihood(model::BayesianLinearModel)
     n = size(X, 2)
     μ_vector = generate_μ(μ, s, X, y)
     V = generate_V(ϕ, s, X, y)
-    IXVXt = I + X * V * transpose(X)
+    IXVXt = cholesky(Hermitian(I + X * V * transpose(X)))
     yXμ = y - X * μ_vector
     return det(IXVXt)^(-1 // 2) *
-           (λ * v +
-            transpose(yXμ) *
-            inv(IXVXt) *
-            (yXμ))^((v + n) / 2)
+        (λ * v +
+        transpose(yXμ) *
+        inv(IXVXt) *
+        (yXμ))^((v + n) / 2)
 end 
