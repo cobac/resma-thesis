ea6bab07a64a0dced107c875ffde1eb20eeab9bc
Coba <coba@cobac.eu>
2022-06-03 10:57:55 +0200

    Fixed imports, exports and BIC for SimpleGGM

--- packages/SimpleGGM/src/SimpleGGM.jl
+++ packages/SimpleGGM/src/SimpleGGM.jl
@@ -1,31 +1,42 @@
 module SimpleGGM
 using Distributions, LinearAlgebra, StatsBase, StatsAPI, DataFrames
+import StatsAPI: fit, bic, coefnames
 
-export ggm
+export ggm, fit, bic, coefnames
 
-struct GGM{M<:AbstractMatrix, D<:AbstractMvNormal} <: StatisticalModel
+struct GGM{M<:AbstractMatrix,D<:AbstractMvNormal} <: StatisticalModel
     x::M
-    dist::D
+    dist::D # Precision matrix
     names::Vector{String}
-end 
+end
 
-function ggm(df::AbstractDataFrame)
+function ggm(df::AbstractDataFrame, target::Union{Nothing,AbstractMatrix} = nothing)
     x = hcat(eachcol(df)...)
-    return fit(GGM, x, names(df))
-end 
+    if isnothing(target)
+        target = ones(size(x, 2), size(x, 2))
+    end
+    return fit(GGM, x, target, names(df))
+end
 
 function StatsBase.fit(::Type{GGM},
-                       x::AbstractMatrix,
-                       names::AbstractVector{<:AbstractString})
-    Κ = Symmetric(inv(cov(x)))
+    x::AbstractMatrix,
+    target::AbstractMatrix,
+    names::AbstractVector{<:AbstractString})
+    issymmetric(target) || error("Target matrix is not symmetric.")
+    size(target) == (size(x, 2), size(x, 2)) ||
+        error("Mismatched dimensions between covariance and target matrices.")
+    Κ = inv(Hermitian(cov(x, corrected = false))) .* target
     # TODO: Assumes centered variables
     dist = MvNormalCanon(Κ)
     return GGM(x, dist, names)
-end 
+end
+
+StatsAPI.bic(model::GGM, x::AbstractMatrix) = length(model.names) * log(size(model.x, 1)) -
+                                              2 * loglikelihood(model.dist, x')
 
 function StatsAPI.coefnames(model::GGM)
     it = Iterators.product(model.names, model.names)
     return [string(i, "--", j) for (j, i) in it if i != j]
-end 
+end
 
 end # module
